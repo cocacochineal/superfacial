bfd53111919fb1cf7a6c98154be514c5a3a397c4
cocacochineal <ltjun_40@163.com>
2023-03-01 22:34:24 +0800

    update app and tupian

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown(
 )
 
 
-st.markdown('# 请选出你感兴趣的面孔！！！！')
+st.markdown('# 请选出你感兴趣的面孔:smile:！！！')
 with st.form("my_form"):
     col1, col2, col3, col4 = st.columns(4)
     options=np.zeros(52)
@@ -42,23 +42,22 @@ with st.form("my_form"):
     with col1:
         for i in c1:
             st.image(Image.open(f'pics/interface_face/{i}.jpg'), width=150)
-            options[i-1]=st.checkbox(f'Like?', key=f"{i}")
+            options[i-1]=st.checkbox(f':heart:', key=f"{i}")
     with col2:
         for i in c2:
             st.image(Image.open(f'pics/interface_face/{i}.jpg'), width=150)
-            options[i-1]=st.checkbox(f'Like?', key=f"{i}")
+            options[i-1]=st.checkbox(f':heart:', key=f"{i}")
     with col3:
         for i in c3:
             st.image(Image.open(f'pics/interface_face/{i}.jpg'), width=150)
-            options[i-1]=st.checkbox(f'Like?', key=f"{i}")
+            options[i-1]=st.checkbox(f':heart:', key=f"{i}")
     with col4:
         for i in c4:
             st.image(Image.open(f'pics/interface_face/{i}.jpg'), width=150)
-            options[i-1]=st.checkbox(f'Like?', key=f"{i}")
+            options[i-1]=st.checkbox(f':heart:', key=f"{i}")
     
     submitted = st.form_submit_button("选好了")
     
-
 if submitted:
     # prediction = requests.post(url, files=params).json()
     #st.write(options)
@@ -69,55 +68,10 @@ if submitted:
     #st.write(requests.get(url))
     form_submit= requests.post(url, data=params).json()
     # st.write(form_submit)
-    
-    url = 'http://localhost:8000/wagon'
-    # url='http://139.198.183.85:5000/wagon/'
-    # make sure requests is using the correct method: post or get
-    prediction = requests.get(url).json()      
-    # st.write(prediction) 
-    pil_image=Image.open('wagon.jpeg')
-    d = ImageDraw.Draw(pil_image)
-    #st.write(prediction)
-    results=prediction[0]
-    face_landmarks_list = prediction[1]
-    print(results)
-    print(face_landmarks_list)
-    first_in=results.index(sorted(results)[-1])
-    i=0
-    snd_in=results.index(sorted(results)[-2])
-    third_in=results.index(sorted(results)[-3])
-    for i in range(0,len(face_landmarks_list)):
-        result =results[i]
-        if i==first_in:
-            color='green'
-            txt="#1 MATCH!"
-        elif i==snd_in:
-            color='green'
-            txt="#2 MATCH"
-        elif i==third_in:
-            color='green'
-            txt="#3 MATCH"
-        else:
-            color='red'
-            txt="not match..."
-        osd = Image.new("RGB", (130,35), color)
-        font = ImageFont.truetype(r'arial.ttf', 20) 
-        dctx = ImageDraw.Draw(osd)  # create drawing context
-        dctx.text((10, 10), txt,  fill="black", font=font) 
-        x=face_landmarks_list[i]['left_eyebrow'] 
-        (a,b)=[sum(y) / len(y) for y in zip(*x)]
-        a=int(a)
-        b=int(b)-35
-        pil_image.paste(
-        osd,
-        box=(a, b, osd.size[0] + a, osd.size[1] + b),
-        mask=Image.new("L", osd.size, 192))
-        i+=1
-    st.image(pil_image)
-    st.balloons()
-    
+    st.title('分析完成')
 
-uploaded_file = st.file_uploader("Predict on your own chosen image")
+
+uploaded_file = st.file_uploader("放入需要匹配的图片")
 if uploaded_file is not None:
     image = uploaded_file.read()
     params = {'image': image}
@@ -187,20 +141,20 @@ if uploaded_file is not None:
             result =results[i]
             if i==first_in:
                 color='green'
-                txt="#1 MATCH!"
+                txt=" "
             elif i==snd_in:
                 color='green'
-                txt="#2 MATCH"
+                txt=" "
             elif i==third_in:
                 color='green'
-                txt="#3 MATCH"
+                txt=" "
             else:
                 color='red'
-                txt="not match..."
-            osd = Image.new("RGB", (100,25), color)
+                txt=""
+            osd = Image.new("RGB", (25,25), color)
             dctx = ImageDraw.Draw(osd)  # create drawing context
             font = ImageFont.truetype(r'arial.ttf', 20) 
-            dctx.text((10, 5), txt,  fill="black", font=font) 
+            # dctx.text((10, 5), txt,  fill="black", font=font) 
             x=face_landmarks_list[i]['left_eyebrow'] 
             (a,b)=[sum(y) / len(y) for y in zip(*x)]
             a=int(a)
